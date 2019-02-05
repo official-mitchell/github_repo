699dd576b704949f6cae4aff6a044698a5597a29
Mitchell Opatowsky <opatowsky77@gamil.com>
2019-02-05 15:28:14 -0500

    lol

---
+++ LearningGit.js
@@ -0,0 +1,21 @@
+
+// Git is a VCS - version control system
+// lets you create different versions of code and swap back and forth
+// other VCSs include Subversion, ...
+
+// Homebrew - package manager for macOS, Yarn is another alternative
+    // is this the same thing as npm?
+    
+    // (brew for 'primary software', yarn for npm packages, npm for backend modules and bower for front-end libraries).
+    
+    // brew: installation of software, i.e. ready to consume applications like wget.
+    // npm: installation of packages (libraries), i.e. pieces of functionality to help you build your own applications.
+    // yarn: also installation of packages.
+    
+        // yarn and npm are both manage module installations and dependencies. Yarn was built to address some of the shortcomings of npm. The biggest advantages of yarn over npm are:
+            // 1. Installing packages with yarn is parallelized and so package installation is faster.
+            // 2. package.json can be very loose in terms of version numbers. yarn.lock (similar to npm shirkwrap) locks this down so that two machines with the same package.json always install the exact same packages.
+            // 3. yarn allows you to check why some packages are installed (understand the dependency tree)
+            // Enhanced Security
+            // Offline mode
+            // Parallel Installation - so fast installation
